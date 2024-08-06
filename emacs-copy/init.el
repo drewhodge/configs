3fc52e4efa49a1b007173d1579ce908f6e82c582
Drew Hodge <drew.hodge@icloud.com>
2024-08-06 10:39:24 -0400

    Minor change to dh-denote-journal function.

--- emacs-copy/init.el
+++ emacs-copy/init.el
@@ -64,6 +64,7 @@
 ;                repo.                                                       ;
 ;                Removed deprecated 'denote-link-buttonize-buffer' function  ;
 ;                --now using 'denote-fontify-links-mode-maybe' (Denote 3.0). ;
+; 06.Aug.2024    Minor correction to dh-denote-journal function.             ;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; Temporary fix for invalid image type issue, until Emacs 29.x is released.
@@ -142,7 +143,7 @@
 
 ;; Tab settings
 (setq-default tab-width 2)
-(setq-default evil-shift-width tab-width)
+;(setq-default evil-shift-width tab-width)
 ; Use spaces instead of tabs.
 (setq-default indent-tabs-mode nil)
 
@@ -317,23 +318,116 @@
 ;; 40 Global settings
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; Evil mode
-(use-package evil
- :ensure t
- :init
- (setq evil-want-integration t) ; Optional, already set to t by default.
- (setq evil-want-keybinding nil)
- :config
- (evil-mode 1))
+;; Meow-edit (modl editing)
+(add-to-list 'load-path "~/gitrepos/emacs-meow")
 
-(use-package evil-collection
- :after evil
- :ensure t
- :config
- ;; Register modes individually, as a list passed to evil-collection-init.
- ;; (evil-collection-init '(calendar dired calc ediff))
- ;; Register modes all at once.
- (evil-collection-init))
+(defun meow-setup ()
+  (setq meow-cheatsheet-layout meow-cheatsheet-layout-qwerty)
+  (meow-motion-overwrite-define-key
+   '("j" . meow-next)
+   '("k" . meow-prev)
+   '("<escape>" . ignore))
+  (meow-leader-define-key
+   ;; SPC j/k will run the original command in MOTION state.
+   '("j" . "H-j")
+   '("k" . "H-k")
+   ;; Use SPC (0-9) for digit arguments.
+   '("1" . meow-digit-argument)
+   '("2" . meow-digit-argument)
+   '("3" . meow-digit-argument)
+   '("4" . meow-digit-argument)
+   '("5" . meow-digit-argument)
+   '("6" . meow-digit-argument)
+   '("7" . meow-digit-argument)
+   '("8" . meow-digit-argument)
+   '("9" . meow-digit-argument)
+   '("0" . meow-digit-argument)
+   '("/" . meow-keypad-describe-key)
+   '("?" . meow-cheatsheet))
+  (meow-normal-define-key
+   '("0" . meow-expand-0)
+   '("9" . meow-expand-9)
+   '("8" . meow-expand-8)
+   '("7" . meow-expand-7)
+   '("6" . meow-expand-6)
+   '("5" . meow-expand-5)
+   '("4" . meow-expand-4)
+   '("3" . meow-expand-3)
+   '("2" . meow-expand-2)
+   '("1" . meow-expand-1)
+   '("-" . negative-argument)
+   '(";" . meow-reverse)
+   '("," . meow-inner-of-thing)
+   '("." . meow-bounds-of-thing)
+   '("[" . meow-beginning-of-thing)
+   '("]" . meow-end-of-thing)
+   '("a" . meow-append)
+   '("A" . meow-open-below)
+   '("b" . meow-back-word)
+   '("B" . meow-back-symbol)
+   '("c" . meow-change)
+   '("d" . meow-delete)
+   '("D" . meow-backward-delete)
+   '("e" . meow-next-word)
+   '("E" . meow-next-symbol)
+   '("f" . meow-find)
+   '("g" . meow-cancel-selection)
+   '("G" . meow-grab)
+   '("h" . meow-left)
+   '("H" . meow-left-expand)
+   '("i" . meow-insert)
+   '("I" . meow-open-above)
+   '("j" . meow-next)
+   '("J" . meow-next-expand)
+   '("k" . meow-prev)
+   '("K" . meow-prev-expand)
+   '("l" . meow-right)
+   '("L" . meow-right-expand)
+   '("m" . meow-join)
+   '("n" . meow-search)
+   '("o" . meow-block)
+   '("O" . meow-to-block)
+   '("p" . meow-yank)
+   '("q" . meow-quit)
+   '("Q" . meow-goto-line)
+   '("r" . meow-replace)
+   '("R" . meow-swap-grab)
+   '("s" . meow-kill)
+   '("t" . meow-till)
+   '("u" . meow-undo)
+   '("U" . meow-undo-in-selection)
+   '("v" . meow-visit)
+   '("w" . meow-mark-word)
+   '("W" . meow-mark-symbol)
+   '("x" . meow-line)
+   '("X" . meow-goto-line)
+   '("y" . meow-save)
+   '("Y" . meow-sync-grab)
+   '("z" . meow-pop-selection)
+   '("'" . repeat)
+   '("<escape>" . ignore)))
+
+(require 'meow)
+(meow-setup)
+(meow-global-mode 1)
+
+;; Evil mode
+;(use-package evil
+; :ensure t
+; :init
+; (setq evil-want-integration t) ; Optional, already set to t by default.
+; (setq evil-want-keybinding nil)
+; :config
+; (evil-mode 1))
+
+;(use-package evil-collection
+; :after evil
+; :ensure t
+; :config
+; ;; Register modes individually, as a list passed to evil-collection-init.
+; ;; (evil-collection-init '(calendar dired calc ediff))
+; ;; Register modes all at once.
+; (evil-collection-init))
 
 ;; Spelling
 (setq ispell-program-name "/usr/local/bin/aspell")
@@ -348,7 +442,7 @@
 ;; Define prefixed key bindings.
 (use-package general
   :config
-  (general-evil-setup t)
+;  (general-evil-setup t)
 
   (general-create-definer dh/leader-key-def
     :keymaps '(normal insert visual emacs)
@@ -1027,7 +1121,7 @@
     "Create an entry tagged 'journal', while prompting for a title."
     (interactive)
     (denote
-     (denote--title-prompt)
+     (denote-title-prompt)
      '("journal")))
 
   ;; Define key bindings.
